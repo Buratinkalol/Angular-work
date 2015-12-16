83375f40800f231743ece86ae140fd9d10c56f1d
Buratinkalol <h12vbn6@gmail.com>
2015-12-16 03:11:37 +0300

    added blocks

---
+++ src/blocks/blocksCtrl.js
@@ -0,0 +1,3 @@
+mainApp.controller('blocksCtrl', function ($scope) {
+    $scope.helloMessage = "So, first of all, this block of my page with header & footer are created by ng-include directive.";
+});
